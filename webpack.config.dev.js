5e98ad4bceed9872edc806b66863fc330a8d331f
Arseni Prokharchyk <arseniprokharchyk@geotab.com>
2017-12-06 11:39:05 -0500

    finish dev environment

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,12 +2,10 @@ import webpack from 'webpack';
 import path from 'path';
 
 export default {
-    debug: true,
     devtool: 'cheap-module-eval-source-map',
-    noInfo: false,
     entry: [
         'eventsource-polyfill',
-        'webpack-hos-middleware/client?reload=true',
+        'webpack-hot-middleware/client?reload=true',
         './src/index'
     ],
     target: 'web',
@@ -21,11 +19,14 @@ export default {
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.LoaderOptionsPlugin({
+            debug: true
+        })
     ],
     module: {
         loaders: [
-            {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+            // {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel-loader']},
             {test: /(\.css)$/, loaders: ['style', 'css']},
             {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
             {test: /.(woff|woff2)$/, loader: 'url?prefix=font/&linit5000'},
